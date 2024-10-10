1ba33992fef4d38df30e1cb861d6d87800c59b21
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-10-10 17:03:43 -0300

    Change to rst2pdf
    Signed-off-by: Leonardo Amaral <contato@leonardoamaral.com.br>

--- utils/makeresume.py
+++
@@ -1,9 +0,0 @@
-import sys
-from markdown_pdf import MarkdownPdf, Section
-
-pdf = MarkdownPdf(toc_level=1)
-pdf.add_section(Section(open(sys.argv[1], encoding='utf-8').read()))
-pdf.meta["title"] = "Curriculum Vitae"
-pdf.meta["author"] = "Leonardo Silva Amaral"
-pdf.meta["creator"] = "CI/CD Pipeline"
-pdf.save(sys.argv[2])
